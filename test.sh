96f07d1ed599cf786d8042b5921586d56a072865
Ove Klinghed <ovekli@Oves-MacBook-Pro-2.local>
2019-12-06 20:36:22 +0100

    init

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Create file"
+echo "Jenkins Slave" > /home/jenkins/jenkins-slave.txt
+cp /home/jenkins/jenkins-slave.txt $WORKSPACE/
+
+echo "workspace: $WORKSPACE"
+ls $WORKSPACE
